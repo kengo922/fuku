e4df9cb94ca77e8e20c5a83d33305742fb3c848d
kengo922 <kengo.matsuno.922@gmail.com>
2020-05-18 22:54:47 +0900

    v3

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,32 +1,96 @@
-{ 
-  document.querySelector('button').addEventListener('click', () => {
-    const targetNode = document.getElementById('target');
+
+function effect() { 
+  var element = document.getElementById("target"); 
+  colors=new Array(10);
+    colors[0]="yellow";
+    colors[1]="red";
+    colors[2]="blue";
+    colors[3]="green";
+    colors[4]="black";
+    colors[5]="brown";
+    colors[6]="gray";
+    colors[7]="orange";
+    colors[8]="purple";
+    colors[9]="white";
+    index=Math.floor(10*Math.random());
+    document.bgColor = colors[index];
+  element.style.backgroundColor = colors[index]; 
+} 
+// if (targetNode.style.contains('white') === true) {
+//   targetNode.style.remove('white');
+// } else {
+//   var colors = new Array(
+//     "yellow",
+//     "red",
+//     "blue",
+//     "green",
+//     "black",
+//     "brown",
+//     "gray",
+//     "orange",
+//     "purple",
+//     "white");
+//     var index = Math.floor(Math.random() * colors.length);
+//     targetNode.style.add = colors[index];
+//   }
+// });
+  
+
+      // <p id="target">ここの色が変わります</p>
+      // <input type="button" value=" ボタン " onclick="changeColor('target');" />
+
+      // function changeColor(idname){
+      //   var obj = document.getElementById(idname);
+      //   obj.style.color = '#ffffff';            //文字色を白にする
+      //   obj.style.backgroundColor = '#ff0000'; 
 
 
-    targetNode.classList.toggle('my-color');
-  });
-}
-colors=new Array(10);
-colors[0]="yellow";
-colors[1]="red";
-colors[2]="blue";
-colors[3]="green";
-colors[4]="black";
-colors[5]="brown";
-colors[6]="gray";
-colors[7]="orange";
-colors[8]="purple";
-colors[9]="white";
-index = Math.floor(10 * Math.random());
-// Math.random()は0 以上 1 未満の範囲内でランダムな浮動小数点を返してくれるのでその値に掛ける10を行います。
-//Math.floor() メソッドは、引数として与えた数以下の最大の整数を返します。(整数表示にする)
-document.bgColor=colors[index];
 
-var imglist = new Array(
-  "flowerA.jpg",
-  "flowerB.jpg",
-  "flowerC.gif",
-  "flowerD.gif" );
-var selectnum = Math.floor(Math.random() * imglist.length);
-var output = "<img src=" + imglist[selectnum] + ">";
-document.write(output);
+
+    // targetNode.className = 'my-color my-border';
+    // targetNode.classList.add('my-color');
+    // if (targetNode.classList.contains('my-color') === true) {
+    //   targetNode.classList.remove('my-color');
+    // } else {
+    //   targetNode.classList.add('my-color');
+    // }
+//     targetNode.classList.toggle('my-color');
+//   });
+// }
+   
+   
+
+
+
+
+
+//     var colors = new Array(
+//     "yellow",
+//     "red",
+//     "blue",
+//     "green",
+//     "black",
+//     "brown",
+//     "gray",
+//     "orange",
+//     "purple",
+//     "white");
+//     var index = Math.floor(Math.random() * colors.length);
+//     document.getElementById("target").style = colors[index];
+
+
+//document.bgColorは、背景色を設定・参照します。
+//この機能は、すでにウェブ標準から削除されています。 将来的にブラウザでサポートされなくなる可能性があるので注意してください。
+
+{/* <img id="image" src="images/1.jpg" alt="" />
+    <script language="javascript" type="text/javascript">
+    function OnButtonClick() {
+     var imglist = new Array(
+      "images/1.jpg",
+      "images/2.jpg",
+      "images/3.jpg",
+      "images/4.jpg" );
+     var selectnum = Math.floor(Math.random() * imglist.length);
+     document.getElementById("image").src = imglist[selectnum];
+    </script>
+    <input type="button" value="押すと画像が表示されます！" onclick="OnButtonClick();"/> */}
