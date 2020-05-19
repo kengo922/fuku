54ae2f7a652d7eb694e40e108b43b07694e07290
kengo922 <kengo.matsuno.922@gmail.com>
2020-05-19 20:37:39 +0900

    v5

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,22 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit()
+  end
+ 
 
 end
