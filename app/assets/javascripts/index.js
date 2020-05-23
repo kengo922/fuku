5b1bd3372dc35a496c975e573d3f1976551a7190
kengo922 <kengo.matsuno.922@gmail.com>
2020-05-23 16:26:29 +0900

    v10

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,13 +1,12 @@
 
-function effect() {  
-  var element = document.getElementById("target"); 
-  var colors = gon.product_color ; //DBの値を取得
-  console.log(colors);
-  // データーベースから引っ張ってきたgonの値は、JSONに直す必要がある
+function effect(){  
+  var element = document.getElementById("head_color"); 
+  var colors = gon.product_color; //DBの値を取得
+  // データーベースから引っ張ってきたgonの値は、JSNに直す必要がある
   let index = Math.floor(Math.random() * colors.length);
-    //colorsに格納された数値をランダムで表示している
-    // document.bgColor = colors[index];
+  //colorsに格納された数値をランダムで表示している
   element.style.backgroundColor = colors[index]; 
-  
 
+  
+  // var element = document.getElementById("#head-color");
 } 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :show, :update]
   
   def index
     @products = Product.last(1)
@@ -23,11 +24,28 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      flash[:alert] = "配色を選択して下さい！"
+      redirect_to edit_product_path
+    end
+  end
+  
   private
   def product_params
     params.require(:product).permit(product_color:[])
   end
  
+  def set_product
+    @product = Product.find(params[:id])
+    # :edit, :update, :showにitem.idを与える必要がある為、記述した
+  end
 
 end
+
 
