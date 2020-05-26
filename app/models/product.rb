ee43ad85c6be762be90ffa2528f6d97383d74800
kengo922 <kengo.matsuno.922@gmail.com>
2020-05-26 19:28:59 +0900

    v18

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_many :colors
+  validates :head_color, 
+  :boy_color, 
+  :leg_color, 
+  :shoes_color, 
+  presence: true
+  
 end
--- db/migrate/20200519023001_create_products.rb
+++ db/migrate/20200519023001_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :head_color
-      t.string :boy_color
-      t.string :leg_color
-      t.string :shoes_color
+      t.string :head_color, null: false
+      t.string :boy_color, null: false
+      t.string :leg_color, null: false
+      t.string :shoes_color, null: false
       t.timestamps
     end
   end
